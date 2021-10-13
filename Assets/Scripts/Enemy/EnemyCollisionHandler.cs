d7015a99f8afda24fc74d56aac62fb0778f238ac
dubu-0 <yudin01@mail.ru>
2021-10-13 18:20:35 +0300

    Добавил врагу эффект взрыва
    Так же теперь храню все инстансы в Runtime Instances, используя transform.parent

--- Assets/Scripts/Enemy/EnemyCollisionHandler.cs
+++ Assets/Scripts/Enemy/EnemyCollisionHandler.cs
@@ -4,8 +4,15 @@ namespace Enemy
 {
     public class EnemyCollisionHandler : MonoBehaviour
     {
+        [SerializeField] private ParticleSystem explosion;
+        [SerializeField] private Transform parentForExplosionInstance;
+        
         private void OnParticleCollision(GameObject other)
         {
+            var instance = Instantiate(explosion, transform.position, Quaternion.identity);
+            instance.transform.parent = parentForExplosionInstance;
+            instance.Play();
+            Destroy(instance, 3f);
             Destroy(transform.root.gameObject);
         }
     }
